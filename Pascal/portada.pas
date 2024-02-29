6abce758030060ec77af0fac510d64f057d92e49
LuisRojasSanchez <luisfranciscorojas05@gmail.com>
2024-02-28 22:50:50 -0600

    si

--- Pascal/portada.pas
+++ Pascal/portada.pas
@@ -2,35 +2,36 @@
 Program Caratulaluisfrancisco;
 
 Uses crt;
+
 Begin
+  TextBackground(14);
   clrscr;
   TextColor(blue);
-  TextBackground(14);
   GoToXY(40,1);
   write('Tecnologico Nacional de Mexico Campus Tlalpan');
-  TextColor(green);
-  GoToXY(40,2);
+  TextColor(11);
+  GoToXY(40,3);
   write('Ingenieria en Tecnologias de la Comunicacion');
   TextColor(red);
-  GoToXY(48,3);
+  GoToXY(48,5);
   write('Rojas Sanchez Luis Francisco');
   TextColor(magenta);
-  GoToXY(48,4);
+  GoToXY(48,7);
   write('Fundamentos de Programacion');
   TextColor(magenta);
-  GoToXY(46,5);
+  GoToXY(46,9);
   write('Colula Porfirio Maria Gabriela');
   TextColor(red);
-  GoToXY(54,6);
+  GoToXY(54,11);
   write('Primer Semestre');
   TextColor(red);
-  GoToXY(58,7);
+  GoToXY(58,13);
   write('Caratula');
   TextColor(blue);
-  GoToXY(57,8);
+  GoToXY(57,15);
   write('L241200016');
   TextColor(black);
-  GoToXY(51,9);
-  write('27 de Febrero del 2024');
+  GoToXY(51,17);
+  Write('27 de Febrero del 2024');
   Readkey;
 End.
